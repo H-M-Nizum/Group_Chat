1b401a5e503b6c6d748343184c7e6953a309f0e0
H-M-Nizum <hmnizum1714032@gmail.com>
2024-09-08 21:43:37 +0600

    [ADD] - Client send data from front-end

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,4 +1,5 @@
 from channels.consumer import SyncConsumer
+from channels.exceptions import StopConsumer
 
 class MyConsumer(SyncConsumer):
     def websocket_connect(self, event):
@@ -8,5 +9,11 @@ class MyConsumer(SyncConsumer):
         })
     def websocket_receive(self, event):
         print("Received Message..", event)
+        self.send({
+            "type": "websocket.send",
+            "text": 'recive data',
+        })
+
     def websocket_disconnect(self, event):
         print("Disconnect websocket..", event)
+        raise StopConsumer()
